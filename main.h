3b767abf6c17028e2e3d2e4a60d065049ef3af2a
denisrands <denisrands@gmail.com>
2023-07-25 11:35:04 -0700

    main.h

--- main.h
+++ main.h
@@ -1,8 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
 
-int _printf(const char *format, ...);
+
+int _printf(const char *format, ...);
 
 
-#endif 
+#endif
