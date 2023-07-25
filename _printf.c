a3f22013aa0bb376c77d78a5bcedc417e4414eb0
1Mashilo <Christophermashilo@gmail.com>
2023-07-25 16:44:15 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,12 +1,14 @@
 #include "main.h"
+
 /**
 *_printf - Printf function
 *@format: format.
-*return: no bytes.
+*return - no_char
 */
+
 int _printf(const char *format, ...)
-{
-int no_char = 0;
+
+{ int no_char = 0;
 va_list argument_list;
 if (format == NULL)
 return (-1);
@@ -16,8 +18,7 @@ while (*format)
 if (*format != '%')
 {
 write(1, format, 1);
-no_char++;
-}
+no_char++; }
 else
 { format++;
 if (*format == '\0')
@@ -27,23 +28,25 @@ if (*format == '%')
 write(1, format, 1);
 no_char++; }
 else if (*format == 'c')
-{
-char c = va_arg(argument_list, int);
+{ char c = va_arg(argument_list, int);
 write(1, &c, 1);
-no_char++;
-}
+no_char++; }
 else if (*format == 's')
-{
-char *str = va_arg(argument_list, char*);
+{ char *str = va_arg(argument_list, char*);
 int str_len = 0;
 while (str[str_len] != '\0')
 str_len++;
 write(1, str, str_len);
-no_char += str_len;
+no_char += str_len; }
+else if (*format == 'd' || *format == 'i')
+{ int d = va_arg(argument_list, int);
+char buf[10];
+int len = snprintf(buf, sizeof(buf), "%d", d);
+write(1, buf, len);
+no_char += len;
 }
 }
-format++;
-}
+format++; }
 va_end(argument_list);
 return (no_char);
 }
