179a24beabe10f4159e0ec6a1941699a5681e7bb
1Mashilo <Christophermashilo@gmail.com>
2023-07-25 15:42:35 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,21 +1,16 @@
 #include "main.h"
-
 /**
-* _printf - Printf function
-* @format: format.
-* Return: Printed chars.
+*_printf - Printf function
+*@format: format.
+*Return: Printed chars.
 */
-
 int _printf(const char *format, ...)
 {
 int no_char = 0;
 va_list argument_list;
-
 if (format == NULL)
-return -1;
-
+return (-1);
 va_start(argument_list, format);
-
 while (*format)
 {
 if (*format != '%')
@@ -26,10 +21,8 @@ no_char++;
 else
 {
 format++;
-
 if (*format == '\0')
 break;
-
 if (*format == '%')
 {
 write(1, format, 1);
@@ -45,10 +38,8 @@ else if (*format == 's')
 {
 char *str = va_arg(argument_list, char*);
 int str_len = 0;
-
 while (str[str_len] != '\0')
 str_len++;
-
 write(1, str, str_len);
 no_char += str_len;
 }
@@ -56,7 +47,5 @@ no_char += str_len;
 format++;
 }
 va_end(argument_list);
-
-return no_char;
+return (no_char);
 }
-
